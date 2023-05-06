d0917446227d657ae751f32a92d249993a1b9545
igmeMarcial <igmemarcial@gmail.com>
2023-05-05 21:54:46 -0500

    first commit

--- algorithms-7/ordenar-emociones/index.ts
+++ algorithms-7/ordenar-emociones/index.ts
@@ -2,5 +2,21 @@ import type { Emocion } from "./types";
 
 export default function ordenarEmociones(emociones: Emocion[], orden: boolean): Emocion[] {
   // TODO: implementar
-  return [];
+  if (emociones.length === 0) {
+    return emociones;
+  }
+  const ordenNumerico = orden ? -1 : 1;
+
+  return emociones.sort((a, b) => {
+    const valoresEmociones: Record<Emocion, number> = {
+      ":D": 5,
+      ":)": 4,
+      ":|": 3,
+      ":(": 2,
+      T_T: 1,
+    };
+    const valorA = valoresEmociones[a];
+    const valorB = valoresEmociones[b];
+    return (valorA - valorB) * ordenNumerico;
+  });
 }
